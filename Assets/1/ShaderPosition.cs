f89cd9edcb807f8e6266ced3532eba9bd27bc18d
Miguel <miguelmpfaria@gmail.com>
2025-04-29 18:24:22 +0100

    Cookies

--- Assets/1/ShaderPosition.cs
+++ Assets/1/ShaderPosition.cs
@@ -6,17 +6,37 @@ using UnityEngine;
 
 public class ShaderPosition : MonoBehaviour
 {
-    public float radius = 1f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float radius = 25f;
+    public float radiusMaxValue = 25f;
+    public float speed = 10f;
+
+    private float targetValue = 25f;
 
     // Update is called once per frame
     void Update()
     {
         Shader.SetGlobalVector("_Position", transform.position);
         Shader.SetGlobalFloat("_Radius", radius);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Increase();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Decrease();
+        }
+
+        radius = Mathf.MoveTowards(radius, targetValue, speed * Time.deltaTime);
+    }
+
+    public void Increase()
+    {
+        targetValue = radiusMaxValue;
+    }
+
+    public void Decrease()
+    {
+        targetValue = 0f;
     }
 }
